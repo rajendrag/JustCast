c046e96b57729ddded238ccb42a0de595cc352ef
rajendrag <it.rajendra@gmail.com>
2014-04-22 21:18:46 -0700

    - Mini player added

---
+++ src/com/rp/justcast/video/VideoLayoutFragment.java
@@ -0,0 +1,24 @@
+package com.rp.justcast.video;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rp.justcast.R;
+
+public class VideoLayoutFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		View v = inflater.inflate(R.layout.video_browser, container, false);
+		return v;
+	}
+}
