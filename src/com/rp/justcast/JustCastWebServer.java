c82f988c02a145c1e2910cdce2948ddddeff8785
rajendrag <it.rajendra@gmail.com>
2014-05-09 00:11:26 -0700

    Added support for partial requests.

--- src/com/rp/justcast/JustCastWebServer.java
+++ src/com/rp/justcast/JustCastWebServer.java
@@ -55,6 +55,7 @@ public class JustCastWebServer extends AbstractWebServer {
 			put("zip", "application/octet-stream");
 			put("exe", "application/octet-stream");
 			put("class", "application/octet-stream");
+			put("vob", "video/mpeg");
 		}
 	};
 
@@ -216,8 +217,8 @@ public class JustCastWebServer extends AbstractWebServer {
 					};
 					fis.skip(startFrom);
 					Log.d(TAG, "Chunked Response Starts["+startFrom+"] ends ["+endAt+"]");
-					//res = createResponse(Response.Status.PARTIAL_CONTENT, mime, fis);
-					res = createResponse(Response.Status.OK, mime, fis);
+					res = createResponse(Response.Status.PARTIAL_CONTENT, mime, fis);
+					//res = createResponse(Response.Status.OK, mime, fis);
 					//res.setChunkedTransfer(true);
 					res.addHeader("Connection", "close");
 					res.addHeader("Content-Length", "" + dataLen);
